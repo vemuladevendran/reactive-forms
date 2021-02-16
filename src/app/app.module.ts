44411a594aac59b6e99982be2b1469cf0e11ecd4
devendran <devendranvemula@gmail.com>
2021-02-16 16:02:19 +0530

    form added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,53 @@
+import { Route } from '@angular/compiler/src/core';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '/signup'
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginComponent,
     SignupComponent,
-    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelectModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
+  // password toggel
+   hide = true;
 
-  constructor() { }
+  //  form details
+   signupForm = new FormGroup({
+    FirstName: new FormControl('devendran', [Validators.required, Validators.minLength(2)]),
+    LastName: new FormControl('vemula', [Validators.required]),
+    MobileNumber: new FormControl('9445296380', [Validators.required, Validators.maxLength(10)]),
+    DOB: new FormControl('', Validators.required),
+    email: new FormControl('devendranvemula@gmail.com', Validators.required),
+    password: new FormControl('deva123', [Validators.required, Validators.minLength(6)]),
+   });
+  constructor(
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  handlesubmit(): void{
+    console.log(this.signupForm.value);
   }
 
 }
