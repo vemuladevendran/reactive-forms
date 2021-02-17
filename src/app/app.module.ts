ea74c6a0a20ea01dc3305c791634df6f2d9a93b0
devendran <devendranvemula@gmail.com>
2021-02-17 08:08:09 +0530

    changes in form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,9 @@ import { MatNativeDateModule } from '@angular/material/core';
 import { MatSelectModule } from '@angular/material/select';
 import { MatIconModule } from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+// import { NgxIntlTelInputModule } from 'ngx-intl-tel-input';
+import {Ng2TelInputModule} from 'ng2-tel-input';
+import {MatToolbarModule} from '@angular/material/toolbar';
 const routes: Routes = [
   {
     path: 'login',
@@ -26,7 +29,7 @@ const routes: Routes = [
   },
   {
     path: '**',
-    redirectTo: '/signup'
+    redirectTo: '/login'
   }
 ];
 
@@ -48,6 +51,8 @@ const routes: Routes = [
     MatSelectModule,
     MatIconModule,
     MatButtonModule,
+    Ng2TelInputModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -9,7 +10,21 @@ export class LoginComponent implements OnInit {
 
   constructor() { }
 
+  hide = true;
+
+  // details
+
+  loginform = new FormGroup({
+    Username: new FormControl('', Validators.required),
+    MobileNumber: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
+
   ngOnInit(): void {
+  }
+
+  handlesubmit(): void {
+    console.log(this.loginform.value);
   }
 
 }
